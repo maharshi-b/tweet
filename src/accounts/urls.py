91de96d75b8207a99ab2ea32fe3307f9195b2ae6
Maharshi <42892220+Maharshi-B@users.noreply.github.com>
2019-11-19 18:56:08 +0530

    Follow button

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic.base import RedirectView
-from .views import UserDetailView
+from .views import UserDetailView, UserFollowView
 app_name = 'profiles'
 urlpatterns = [
 
     path('<str:username>/', UserDetailView.as_view(), name="detail"),
+    path('<str:username>/follow', UserFollowView.as_view(), name="follow"),
     # path('create/', TweetCreateApiView.as_view(), name='create'),
     # path('<int:pk>/edit', TweetUpdateView.as_view(), name="update"),
     # path('<int:pk>/delete', TweetDeleteView.as_view(), name="delete"),
