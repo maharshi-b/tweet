7783770ec3ba6312dbe1d68a8879632f4c571baa
Maharshi <42892220+Maharshi-B@users.noreply.github.com>
2019-12-02 01:37:46 +0530

    like functionality

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.views.generic.base import RedirectView
 from .views import (
-    TweetListApiView, TweetCreateApiView, RetweetAPIView
+    TweetListApiView, TweetCreateApiView, RetweetAPIView, LikeToggleAPIView
 )
 app_name = 'tweet-api'
 urlpatterns = [
     path('', TweetListApiView.as_view(), name="list"),
-    path('<int:pk>/retweet', RetweetAPIView.as_view(), name="retweet"),
+    path('<int:pk>/retweet/', RetweetAPIView.as_view(), name="retweet"),
+    path('<int:pk>/like/', LikeToggleAPIView.as_view(), name="like"),
     path('create/', TweetCreateApiView.as_view(), name='create'),
     # path('<int:pk>/edit', TweetUpdateView.as_view(), name="update"),
     # path('<int:pk>/delete', TweetDeleteView.as_view(), name="delete"),
--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -8,6 +8,19 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
+class LikeToggleAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, pk, format=None):
+        tweet_qs = Tweet.objects.filter(pk=pk)
+        message = "Not Allowed"
+        if request.user.is_authenticated:
+            is_liked = Tweet.objects.like_toggle(
+                request.user, tweet_qs.first())
+            return Response({'liked': is_liked})
+        return Response({'message': message}, status=400)
+
+
 class RetweetAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
